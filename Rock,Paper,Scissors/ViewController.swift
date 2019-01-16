477332a42b8313ef3ccbf58666b0deabb93a561b
EDUARDO MENDOZA <rl238513@rlas-macair-25.local>
2019-01-16 11:55:19 -0600

    suicideMKII

--- Rock,Paper,Scissors/ViewController.swift
+++ Rock,Paper,Scissors/ViewController.swift
@@ -10,67 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var countNumber: UILabel!
     @IBOutlet weak var systemChoice: UIImageView!
     @IBOutlet weak var winnerLabel: UILabel!
     @IBOutlet weak var winnerImage: UIImageView!
     @IBOutlet weak var backgroundImmage: UIImageView!
     
     var pictureArray = [#imageLiteral(resourceName: "paper.png"), #imageLiteral(resourceName: "rock.png"), #imageLiteral(resourceName: "scissors.png")]
+    let number: [Int] = [0, 1, 2,]
     
-    let randomInt = Int.random(in: 0...2)
-    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-     backgroundImmage.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.RawValue(UInt8(UIView.AutoresizingMask.flexibleBottomMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleHeight.rawValue) | UInt8(UIView.AutoresizingMask.flexibleRightMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleLeftMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleTopMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleWidth.rawValue)))
         self.backgroundImmage.layer.cornerRadius = self.backgroundImmage.frame.width / 2
         backgroundImmage.backgroundColor = .green
         self.view.backgroundColor = .red
-         winnerImage.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.RawValue(UInt8(UIView.AutoresizingMask.flexibleBottomMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleHeight.rawValue) | UInt8(UIView.AutoresizingMask.flexibleRightMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleLeftMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleTopMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleWidth.rawValue)))
-         systemChoice.autoresizingMask = UIView.AutoresizingMask(rawValue: UIView.AutoresizingMask.RawValue(UInt8(UIView.AutoresizingMask.flexibleBottomMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleHeight.rawValue) | UInt8(UIView.AutoresizingMask.flexibleRightMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleLeftMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleTopMargin.rawValue) | UInt8(UIView.AutoresizingMask.flexibleWidth.rawValue)))
+    }
+    
+    @IBAction func daRules(_ sender: Any) {
+    
+        if let url = URL(string: "https;//www.wrpsa.com/the-official-rules-of-rock-paper-scissors/") {
+            UIApplication.shared.open(url, options: [:])
+        }
     }
     
     @IBAction func paperButton(_ sender: Any) {
-        countReset()
         systemPicture()
     }
     
     @IBAction func rockButton(_ sender: Any) {
-        countReset()
         systemPicture()
     }
     
     @IBAction func scissorButton(_ sender: Any) {
-        countReset()
         systemPicture()
     }
     
     func systemPicture() {
-//        countReset()
-        systemChoice.image = pictureArray[count]
-    }
-    
-    func countReset(){
-        countNumber.resignFirstResponder()
-        if count == 0 {
-            systemPicture()
-            count += 1
-        }
-        else if count == 1 {
-            systemPicture()
-            count += 1
-        }
-       else if count == 2 {
-            count += 1
-            systemPicture()
-            count = 0
-            Int(countNumber.text!)
-            countNumber.text = String(count)
-        }
-//        else if count == 3{
-//            count = 0
-//
-//        }
+        let randomNumber = number.randomElement()!
+        systemChoice.image = pictureArray[randomNumber]
     }
     
     
